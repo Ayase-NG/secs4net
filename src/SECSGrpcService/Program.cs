19b59a15472036afd08d49a60eff5d3bc74a69e2
张金虎 <jh.zhang@hzgy-tech.com.cn>
2026-04-22 16:58:15 +0800

    修改proto的package，与GWP统一

--- src/SECSGrpcService/Program.cs
+++ src/SECSGrpcService/Program.cs
@@ -51,8 +51,8 @@ if (app.Environment.IsDevelopment())
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 Console.WriteLine($"SECSGrpcService 已启动，监听: {(grpcUseHttps ? "https" : "http")}://{grpcHost}:{grpcPort}");
-Console.WriteLine("可测试入口: GY.SECS.ReportGrpcService/ResultReport");
-Console.WriteLine("可测试入口: GY.SECS.ReportGrpcService/ReportAlarm");
+Console.WriteLine("可测试入口: GY.EFEM.ReportGrpcService/ResultReport");
+Console.WriteLine("可测试入口: GY.EFEM.ReportGrpcService/ReportAlarm");
 Console.WriteLine("如需持续监听SECS PrimaryMessage，请在配置中设置 SecsListener:Enabled=true");
 
 app.Run();
